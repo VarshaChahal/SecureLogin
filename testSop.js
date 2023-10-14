bee4fbe63ea1cb6faff9a1bfbd156131e65c56c0
Varsha Chahal <varshachahal20@gmail.com>
2023-10-14 12:26:56 -0700

    Update testSop.js

--- testSop.js
+++ testSop.js
@@ -1,2 +1,4 @@
 console.log("hey there from github");
 console.log("document.location value:", document.location);
+let element = document.getElementById("secret");
+console.log("secret element value is: ",element.value);
